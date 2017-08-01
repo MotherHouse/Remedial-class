7877f5d0e4012fcba6deeb794ae171cf4724a0e6
rehellinen <912377791@qq.com>
2017-08-01 15:12:09 +0800

    database

--- back/Application/Admin/Conf/db.php
+++ back/Application/Admin/Conf/db.php
@@ -8,11 +8,10 @@
 
 return array(
     'DB_TYPE' => 'mysql',
-    'DB_HOST' => '5971951159b09.gz.cdb.myqcloud.com:5136',
-    //内网10.66.194.34
-    //外网5971951159b09.gz.cdb.myqcloud.com:5136
-    'DB_USER' => 'admin',
-    'DB_PWD' => 'a12345678',
+    'DB_HOST' => '',
+
+    'DB_USER' => '',
+    'DB_PWD' => '',
     'DB_PORT' => 3306,
     'DB_NAME' => 'test',
     'DB_CHARSET' => 'utf8',
